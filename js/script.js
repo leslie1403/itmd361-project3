a408c39200aada00f196ad8560adc012fb67670d
leslie1403 <lesquivel@hawk.iit.edu>
2024-04-09 23:05:41 -0500

    removing function

--- js/script.js
+++ js/script.js
@@ -1,11 +1,10 @@
 function init() {
   alert('Leslie Esquivel');
-  function initMap(){
       var map = new google.maps.Map(document.getElementById('map'),
       {center: {lat:-34.397, lng:150.644},
       zoom:8 
     });
-  }
+  
 }
 
 google.maps.event.addDomListener(window,'load', init);
