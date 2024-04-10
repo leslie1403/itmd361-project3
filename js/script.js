0b04ad42ecbd91ce92881d36940e0206d514cab6
leslie1403 <lesquivel@hawk.iit.edu>
2024-04-09 22:49:15 -0500

    create and declare map within InitMap

--- js/script.js
+++ js/script.js
@@ -1,12 +1,10 @@
 function init() {
   alert('Leslie Esquivel');
-  var map;
   function initMap()
-      map = new google.maps.Map(document.getElementById('map'),
+      var map = new google.maps.Map(document.getElementById('map'),
       {center: {lat:-34.397, lng:150.644},
       zoom:8 
     });
-  initMap();
 }
 
 google.maps.event.addDomListener(window,'load', init);
