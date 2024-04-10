774f6d8afbb0faad6daa5c2a49fc278a50475cad
leslie1403 <lesquivel@hawk.iit.edu>
2024-04-09 22:25:08 -0500

    added API key

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 function init() {
   alert('Leslie Esquivel');
-  //var map;
-  //function initMap()
-    //map = new google.maps.Map(document.getElementById('map'),
-      //{center: {lat:-34.397, lng:150.644},
-      //zoom:8 
-    //});
+  var map;
+  function initMap()
+      map = new google.maps.Map(document.getElementById('map'),
+      {center: {lat:-34.397, lng:150.644},
+      zoom:8 
+    });
 }
 google.maps.event.addDomListener(window,'load', init);
