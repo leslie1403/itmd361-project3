b5aa9024640621bcb05cbc568625638d3c75c8e9
leslie1403 <lesquivel@hawk.iit.edu>
2024-04-10 00:23:07 -0500

    redo, using demo code for simple map

--- js/script.js
+++ js/script.js
@@ -1,34 +1,8 @@
-function init(){
-  //alert('it works');
-  var el = document.getElementById('map');
-  var myLocation = new google.maps.LatLng(41.835117, -87.627130);
-  var mapOptions = {
-    center: myLocation,
-    zoom: 18,
-    mapTypeId: google.maps.MapTypeId.SATELLITE,
-    mapTypeControlOptions: {
-      position: google.maps.ControlPosition.BOTTOM_CENTER
-    }
-  };
-
-  var myMap = new google.maps.Map(el, mapOptions);
-
-  var marker = new google.maps.Marker({
-    position: myLocation,
-    map: myMap,
-    animation: google.maps.Animation.BOUNCE,
-    icon: './media/cursor/iit-icon.png'
-  });
-
-  var contentString = '<h1>IIT</h1><p>This is Illinois Institute of Technology</p>';
-
-  var infowindow = new google.maps.InfoWindow({
-      content: contentString
-    });
-
-  google.maps.event.addListener(marker, 'mouseover', function() {
-      infowindow.open(myMap, marker);
+function initMap(){
+  var map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: -34.397, lng: 150.644},
+      zoom: 8
     });
 }
 
-google.maps.event.addDomListener(window,'load', init);
+google.maps.event.addDomListener(window, 'load', initMap);
