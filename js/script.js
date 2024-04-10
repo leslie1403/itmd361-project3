003386aec5cb3fececc877b5a1c4372b9ec91c28
leslie1403 <lesquivel@hawk.iit.edu>
2024-04-09 23:32:23 -0500

    in class example test

--- js/script.js
+++ js/script.js
@@ -1,8 +1,34 @@
-var map;
-function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -34.397, lng: 150.644},
-    zoom: 8
+function init(){
+  //alert('it works');
+  var el = document.getElementById('canvas');
+  var myLocation = new google.maps.LatLng(41.835117, -87.627130);
+  var mapOptions = {
+    center: myLocation,
+    zoom: 18,
+    mapTypeId: google.maps.MapTypeId.SATELLITE,
+    mapTypeControlOptions: {
+      position: google.maps.ControlPosition.BOTTOM_CENTER
+    }
+  };
+
+  var myMap = new google.maps.Map(el, mapOptions);
+
+  var marker = new google.maps.Marker({
+    position: myLocation,
+    map: myMap,
+    animation: google.maps.Animation.BOUNCE,
+    icon: './media/cursor/iit-icon.png'
   });
+
+  var contentString = '<h1>IIT</h1><p>This is Illinois Institute of Technology</p>';
+
+  var infowindow = new google.maps.InfoWindow({
+      content: contentString
+    });
+
+  google.maps.event.addListener(marker, 'mouseover', function() {
+      infowindow.open(myMap, marker);
+    });
 }
-google.maps.event.addDomListener(window,'load', initMap);
+
+google.maps.event.addDomListener(window,'load', init);
