e4c69d16a319b5fa729ea1ad97aabf065b3d2443
leslie1403 <lesquivel@hawk.iit.edu>
2024-04-09 22:52:22 -0500

    adding missing }

--- js/script.js
+++ js/script.js
@@ -1,10 +1,11 @@
 function init() {
   alert('Leslie Esquivel');
-  function initMap()
+  function initMap(){
       var map = new google.maps.Map(document.getElementById('map'),
       {center: {lat:-34.397, lng:150.644},
       zoom:8 
     });
+  }
 }
 
 google.maps.event.addDomListener(window,'load', init);
